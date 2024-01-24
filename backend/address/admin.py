4018113f2ff79499f881c47fc4d0e6e298ff1ff6
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-01-24 10:21:10 +0600

    feat: address model created

---
+++ backend/address/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+from .models import Address
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "unit_number",
+        "street_number",
+        "address_line1",
+        "address_line2",
+        "city",
+        "region",
+        "postal_code",
+        "country",
+    )
+    list_display_links = (
+        "unit_number",
+        "street_number",
+        "address_line1",
+        "address_line2",
+        "city",
+        "region",
+        "postal_code",
+        "country",
+    )
+    search_fields = (
+        "unit_number",
+        "street_number",
+        "address_line1",
+        "address_line2",
+        "city",
+        "region",
+        "postal_code",
+        "country",
+    )
+    list_per_page = 25
+
+admin.site.register(Address, AddressAdmin)
---
+++ backend/address/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from country.models import Country
+
+
+class Address(models.Model):
+    unit_number = models.CharField(max_length=100, null=True, blank=True)
+    street_number = models.CharField(max_length=100, null=True, blank=True)
+    address_line1 = models.TextField()
+    address_line2 = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.address_line1
+
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Addresses"
+        db_table = "address"
