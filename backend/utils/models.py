51826e135880012dc359b0405ae8ae1a412885f5
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-01-24 09:38:48 +0600

    utils model created

---
+++ backend/utils/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class CommonInfo(models.Model):
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
