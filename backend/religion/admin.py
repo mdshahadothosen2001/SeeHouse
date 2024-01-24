c14a707898ab095b55a86cbefe5730fa41b27043
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-01-24 10:12:09 +0600

    feat: religion model created

---
+++ backend/religion/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Religion
+
+
+class ReligionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "religion_name",
+    )
+    list_display_links = (
+        "religion_name",
+    )
+    search_fields = (
+        "religion_name",
+    )
+    list_per_page = 25
+
+admin.site.register(Religion, ReligionAdmin)
---
+++ backend/religion/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Religion(models.Model):
+    religion_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.religion_name
+    
+    class Meta:
+        verbose_name = "Religion"
+        verbose_name_plural = "Religions"
+        db_table = "religion"
