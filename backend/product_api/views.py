0e376f31d7de8474583e5a013db0092c89d2eb2c
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-01-30 22:35:00 +0600

    refactor: change about product API

--- backend/product_api/views.py
+++ backend/product_api/views.py
@@ -2,9 +2,10 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 
-from category.models import Category
+from subcategory.models import Subcategory
+from product.models import Product
 
 
 class ProductView(APIView):
@@ -14,33 +15,25 @@ class ProductView(APIView):
 
     def get(self, request, *args, **kwargs):
 
-        categories = get_list_or_404(Category)
+        subcategory_id = request.data.get("subcategory")
+        if not subcategory_id:
+            raise ValueError("Must Required subcategory id")
+        
+        subcategory_data = get_object_or_404(Subcategory, id=subcategory_id)
+        
+        products = get_list_or_404(Product)
+        products_data = []
+    
+        for product in products:
+            products_data += [{
+                "id":product.id,
+                "shop_name":product.shop.shop_name,
+                "product_name":product.product_name,
+                "product_code":product.product_code,
+                "description":product.description,
+                "stock":product.stock,
+                "price":product.price,
+                "rating":product.rating
+            }]
 
-        product_response_data = {"categories": []}
-
-        for category in categories:
-            subcategories_data = []
-
-            for subcategory in category.subcategory.all():
-                products_data = []
-
-                for product in subcategory.product.all():
-                    product_data = {
-                        "product_name": product.product_name,
-                        "product_code": product.product_code,
-                    }
-                    products_data.append(product_data)
-
-                subcategory_data = {
-                    subcategory.subcategory_name: products_data
-                }
-                subcategories_data.append(subcategory_data)
-
-            category_data = {
-                "category_name": category.category_name,
-                "subcategory_names": subcategories_data
-            }
-
-            product_response_data["categories"].append(category_data)
-
-        return Response({"product":product_response_data})
+        return Response({"product":products_data})
