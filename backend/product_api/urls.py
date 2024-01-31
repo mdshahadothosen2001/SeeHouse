e52fd4e438f59b710021f4fe10a36e3be81ff3cb
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-01-31 12:15:27 +0600

    feat: product subcategory API
    This API response each subcategory id and subcategory name list

--- backend/product_api/urls.py
+++ backend/product_api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     ProductView,
     ProductCategoryView,
+    ProductSubCategoryView
     )
 
 urlpatterns = [
@@ -12,10 +13,18 @@ urlpatterns = [
         view=ProductView.as_view(),
         name="products"
     ),
+
     # GET localhost/product/category/
     path(
         route="category",
         view=ProductCategoryView.as_view(),
         name="product_category"
+    ),
+    
+    # GET localhost/product/category/subcategory/
+    path(
+        route="category/subcategory/",
+        view=ProductSubCategoryView.as_view(),
+        name="product_subcategory"
     ),
 ]
--- backend/product_api/views.py
+++ backend/product_api/views.py
@@ -1,8 +1,9 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_list_or_404
 
 from subcategory.models import Subcategory
 from category.models import Category
@@ -16,13 +17,11 @@ class ProductView(APIView):
 
     def get(self, request, *args, **kwargs):
 
-        subcategory_id = request.data.get("subcategory")
+        subcategory_id = request.data.get("subcategory_id")
         if not subcategory_id:
-            raise ValueError("Must Required subcategory id")
+            raise ValidationError("Must Required subcategory_id")
         
-        subcategory_data = get_object_or_404(Subcategory, id=subcategory_id)
-        
-        products = get_list_or_404(Product)
+        products = get_list_or_404(Product, subcategory=subcategory_id)
         products_data = []
     
         for product in products:
@@ -48,6 +47,7 @@ class ProductCategoryView(APIView):
     def get(self, request, *args, **kwargs):
 
         categories = get_list_or_404(Category)
+
         category_data = []
         for category in categories:
             category_data += [
@@ -57,3 +57,28 @@ class ProductCategoryView(APIView):
                 }
             ]
         return Response(category_data)
+
+
+class ProductSubCategoryView(APIView):
+    """This class take 'category_id' one argument and then response subcategory list """
+
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+
+        category_id = request.data.get("category_id")
+        if not category_id:
+            raise ValidationError("Must Required category_id")
+
+        subcategories = get_list_or_404(Subcategory, category=category_id)
+
+        subcategory_data = []
+        for subcategory in subcategories:
+            subcategory_data += [
+                {
+                    "id":subcategory.id,
+                    "subcategory_name":subcategory.subcategory_name
+                }
+            ]
+
+        return Response({"subcategory":subcategory_data})
