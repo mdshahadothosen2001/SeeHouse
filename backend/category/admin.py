5047d69b72d896d6e6cbaeae94ec94f23072ca4e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-02-17 17:03:54 +0600

    chore: category model modified

--- backend/category/admin.py
+++ backend/category/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import CategoryModel
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -13,4 +13,4 @@ class CategoryAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(CategoryModel, CategoryAdmin)
--- backend/category/models.py
+++ backend/category/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class CategoryModel(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
