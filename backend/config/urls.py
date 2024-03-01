0b9f14f37605ca8563dc70f5acf0df30bbb39755
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-03-01 17:30:38 +0600

    chore: seehouse port and debug toolbar  modified

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,3 +25,10 @@ urlpatterns = [
         name="product",
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
