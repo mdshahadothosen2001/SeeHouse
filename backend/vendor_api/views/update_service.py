e8bb9b41e697c54125207e969d42087f1584c7ad
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2024-04-07 11:33:38 +0600

    chore: update vendor service api modified

--- backend/vendor_api/views/update_service.py
+++ backend/vendor_api/views/update_service.py
@@ -1,29 +1,27 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_200_OK
+from rest_framework import status
 
 from shop.models import ShopModel
 from utils.utils import tokenValidation
+from utils.custom_permission import IsVendor
 from ..serializers.update_service import UpdateServiceByVendorSerializer
 
 
 class UpdateServiceByVendorView(APIView):
     """This class permit to update their information about service"""
 
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsVendor]
 
     def patch(self, request, *args, **kwargs):
         payload = tokenValidation(request)
-        if payload.get("is_vendor") == True:
-
-            instance = ShopModel.objects.get(vendor=payload.get("user_id"))
-            serializer = UpdateServiceByVendorSerializer(
-                instance=instance, data=request.data, partial=True
+        instance = ShopModel.objects.get(vendor=payload.get("user_id"))
+        serializer = UpdateServiceByVendorSerializer(
+            instance=instance, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                "Your shop information has been updated!", status=status.HTTP_200_OK
             )
-            if serializer.is_valid():
-                serializer.save()
-                return Response("Your shop information has been updated!")
-            else:
-                return Response("Please provide valid data")
-        return Response("You have no permission to update product")
+        return Response(status=status.HTTP_400_BAD_REQUEST)
